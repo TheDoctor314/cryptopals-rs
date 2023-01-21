1146f96b8efd843c921395003d59f121274fb04a
TheDoctor314 <64731940+TheDoctor314@users.noreply.github.com>
2023-01-21 21:02:51 +0530

    Finish set 1, challenge 8

--- tests/set1.rs
+++ tests/set1.rs
@@ -111,3 +111,29 @@ fn challenge7() -> Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn challenge8() -> Result<()> {
+    let reader = BufReader::new(File::open("testdata/set1/8.txt")?);
+
+    const BLOCK_SIZE: usize = 16;
+
+    let lines: Vec<_> = reader.lines().collect::<Result<_, _>>()?;
+    let candidate = lines
+        .iter()
+        .map(|line| line.as_bytes())
+        .find(|line| {
+            let num_blocks = line.len() / (2 * BLOCK_SIZE);
+
+            use std::collections::HashSet;
+            let set: HashSet<_> = line.chunks_exact(2 * BLOCK_SIZE).collect();
+
+            set.len() != num_blocks
+        })
+        .expect("Should have found an aes-128-ecb ciphertext");
+
+    let candidate = std::str::from_utf8(candidate)?;
+    expect!["d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a"].assert_eq(candidate);
+
+    Ok(())
+}
